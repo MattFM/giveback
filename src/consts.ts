5d454b943469127711c1b86b9af2457fd0cd6712
Matt Morelli <mmorelli@gmail.com>
2025-01-22 15:45:46 +0000

    Package updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Giveback';
+export const SITE_TAGLINE = 'Give back to the people and places you visit'; // site tagline
+export const SITE_DESCRIPTION = 'Giveback is a growing collection of animal sanctuaries, beach cleans, cultural exchanges, social impact projects, tours, tree planting, wellbeing experiences and more'; // site meta description for SEO
